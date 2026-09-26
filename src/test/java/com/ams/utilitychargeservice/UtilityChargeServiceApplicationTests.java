3e17ee0336b8b5c7470ac28cfa1425fec62da64a
DilumTharinda <tharindadilum6@gmail.com>
2026-09-26 19:43:39 +0530

    fix: remove duplicate ActiveProfiles annotation

--- src/test/java/com/ams/utilitychargeservice/UtilityChargeServiceApplicationTests.java
+++ src/test/java/com/ams/utilitychargeservice/UtilityChargeServiceApplicationTests.java
@@ -2,10 +2,10 @@ package com.ams.utilitychargeservice;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
-@org.springframework.test.context.ActiveProfiles("test")
-@org.springframework.test.context.ActiveProfiles("test")
+@ActiveProfiles("test")
 class UtilityChargeServiceApplicationTests {
 
     @Test
